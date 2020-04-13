34a11376341f56094391221edcfb3b8f47ed7af1
monikaklim <monyklim@gmail.com>
2020-04-13 16:56:00 +0200

    passing recupe data with property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
   title = 'project';
+  onNavigate(feature:string){
+    this.loadedFeature = feature;
+  }
 }
