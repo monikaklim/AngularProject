54edc87dc5818f03dd95520e95b03d2e11510ba7
monikaklim <monyklim@gmail.com>
2020-04-14 12:58:51 +0200

    added ingredients to recipe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,18 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
 recipeSelected = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] = [
-        new Recipe('Test','test description','https://c0.wallpaperflare.com/preview/40/957/185/canada-calgary-food-healthy-eating.jpg'),
-        new Recipe('Test2','test description2','https://c0.wallpaperflare.com/preview/40/957/185/canada-calgary-food-healthy-eating.jpg'),
-        new Recipe('Test3','test description3','https://c0.wallpaperflare.com/preview/40/957/185/canada-calgary-food-healthy-eating.jpg'),
-        new Recipe('Test4','test description4','https://c0.wallpaperflare.com/preview/40/957/185/canada-calgary-food-healthy-eating.jpg')
+        new Recipe('Test','test description','https://c0.wallpaperflare.com/preview/40/957/185/canada-calgary-food-healthy-eating.jpg',
+        [new Ingredient('meat',2),new Ingredient('bread',1)]),
+        new Recipe('Test2','test description2','https://c0.wallpaperflare.com/preview/40/957/185/canada-calgary-food-healthy-eating.jpg',
+        [new Ingredient('eggs',3)]),
+        new Recipe('Test3','test description3','https://c0.wallpaperflare.com/preview/40/957/185/canada-calgary-food-healthy-eating.jpg',
+        [new Ingredient('salt',1),new Ingredient('tomato',1),new Ingredient('salad',2)])
+      
       ]
 
       getRecipes(){
