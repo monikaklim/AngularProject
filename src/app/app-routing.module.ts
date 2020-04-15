9842e45625802cce3e49524698711ed1f8b2700d
monikaklim <monyklim@gmail.com>
2020-04-15 14:59:44 +0200

    routing terminated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 const routes: Routes = [
   {path:'', redirectTo:'/recipes', pathMatch: 'full'},
   {path:'recipes', component: RecipesComponent, children:[
     {path:'', component: RecipeStartComponent},
-    {path:':id', component:RecipeDetailComponent}
+    {path:'new', component: RecipeEditComponent},
+    {path:':id', component:RecipeDetailComponent},
+    {path:':id/edit', component: RecipeEditComponent}
   ]
 },
   {path:'shopping-list', component: ShoppingListComponent}
