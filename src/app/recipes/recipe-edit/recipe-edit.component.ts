d34a7857427d349524b7581ce360c2dad3f87c01
monikaklim <monyklim@gmail.com>
2020-04-17 11:29:58 +0200

    added validation

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,7 +2,7 @@ import { Ingredient } from './../../shared/ingredient.model';
 import { RecipeService } from './../recipe.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -40,17 +40,17 @@ private initForm(){
       if(recipe['ingredients']){
         for(let ingredient of recipe.ingredients){
           recipeIngredients.push(new FormGroup({
-            'name': new FormControl(ingredient.name),
-            'amount': new FormControl(ingredient.amount)
+            'name': new FormControl(ingredient.name,Validators.required),
+            'amount': new FormControl(ingredient.amount,[Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
           }));
         }
       }
 
   }
   this.recipeForm = new FormGroup({
-    'name': new FormControl(recipeName),
-    'imagePath': new FormControl(recipeImagePath),
-    'description': new FormControl(recipeDescription),
+    'name': new FormControl(recipeName, Validators.required),
+    'imagePath': new FormControl(recipeImagePath,Validators.required),
+    'description': new FormControl(recipeDescription,Validators.required),
     'ingredients': recipeIngredients
   })
 }
@@ -63,5 +63,14 @@ onSubmit(){
 get controls(){
   return (<FormArray>this.recipeForm.get('ingredients')).controls;
 }
+
+
+onAddIngredient(){
+  this.controls.push(new FormGroup({
+    'name': new FormControl(null,Validators.required),
+    'amount': new FormControl(null,[Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+  }));
+}
+
 
 }
